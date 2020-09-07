6079919b6470e1fa4ca5f91b485cdfa1ef3a3ee7
Regan Iu <reganiu@hotmail.com>
2020-09-07 17:05:01 -0400

    added code to intro part of website

--- src/sections/Intro/Intro.js
+++ src/sections/Intro/Intro.js
@@ -1,9 +1,20 @@
 import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import "./Intro.css"
 
-function Intro () {
-    <div>
-        <p> hi this is the intro</p>
-    </div>
+class Intro extends React.Component {
+    render() {
+
+
+        return (
+            <div className="jumbotron-fluid bg-dark text-center" id="jumbotron">
+                <div className="intro">
+                    <h1 className="intro-text" id="name">Hey, I'm Regan!</h1>
+                </div>
+
+            </div>
+        )
+    }
 }
 
 export default Intro
