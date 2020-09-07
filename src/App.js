965ad1d9720c907bc1caedcab74be0a2419a5320
Regan Iu <reganiu@hotmail.com>
2020-09-07 17:03:52 -0400

    added intro component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Navigation from './sections/NavBar/Navigation';
+import Intro from './sections/Intro/Intro';
+
 
 class App extends React.Component {
   render () {
+    
     return (
       <div>
+        
         <Router>
-        <Route path="/" exact component={Navigation} />
+          <Route path="/" exact component={Intro} />
+          <Route path="/" exact component={Navigation} />
 
 
         </Router>
