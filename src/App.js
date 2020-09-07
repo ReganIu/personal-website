f3ee6a9195d0462e7e6f17ab524030a7640251b7
Regan Iu <reganiu@hotmail.com>
2020-09-07 19:12:37 -0400

    added About section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Navigation from './sections/NavBar/Navigation';
 import Intro from './sections/Intro/Intro';
+import About from './sections/About/About';
 
 
 class App extends React.Component {
@@ -13,6 +14,7 @@ class App extends React.Component {
         <Router>
           <Route path="/" exact component={Intro} />
           <Route path="/" exact component={Navigation} />
+          <Route path="/" exact component={About} />
 
 
         </Router>
