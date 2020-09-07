465c570eacbb4525298ff945a1ccb197dc5c3bf3
Regan Iu <reganiu@hotmail.com>
2020-09-07 19:12:47 -0400

    code changes

--- src/sections/Intro/Intro.js
+++ src/sections/Intro/Intro.js
@@ -7,7 +7,7 @@ class Intro extends React.Component {
 
 
         return (
-            <div className="jumbotron-fluid bg-dark text-center" id="jumbotron">
+            <div className="jumbotron-fluid bg-dark text-center" id="jumbotronIntro">
                 <div className="intro">
                     <h1 className="intro-text" id="name">Hey, I'm Regan!</h1>
                 </div>
