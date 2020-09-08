9ff40e6542fbba28791a886e64d4fa772983ec21
Regan Iu <reganiu@hotmail.com>
2020-09-07 23:29:33 -0400

    added animated intro text

--- src/sections/Intro/Intro.js
+++ src/sections/Intro/Intro.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import "./Intro.css"
+import "./IntroAnimated.css"
 
 class Intro extends React.Component {
     render() {
 
 
         return (
-            <div className="jumbotron-fluid bg-dark text-center" id="jumbotronIntro">
+            <div className="jumbotron-fluid " id="jumbotronIntro">
                 <div className="intro">
                     <h1 className="intro-text" id="name">Hey, I'm Regan!</h1>
                 </div>
