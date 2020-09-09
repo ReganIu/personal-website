3830883b4f5df6f8713b0aa4483851497035092e
Regan Iu <reganiu@hotmail.com>
2020-09-09 00:10:06 -0400

    added projects.js code

---
+++ src/sections/Projects/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Projects.css'
+
+class Projects extends React.Component {
+    render(){
+        return (
+            <div>
+                <div className='jumbotron-fluid' id="jumbotronProjects">
+                    <h1 id="projectWord">Projects</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Projects
