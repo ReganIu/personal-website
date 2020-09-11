7eaf772d45acfc5af89921da7316bb4c140a60b9
Regan Iu <reganiu@hotmail.com>
2020-09-10 23:49:40 -0400

    added Project Card components

--- src/sections/Projects/Projects.js
+++ src/sections/Projects/Projects.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import './Projects.css'
+import Button from "react-bootstrap/Button"
+import Card from "react-bootstrap/Card"
 
 class Projects extends React.Component {
+
+    // constructor method to bind the methods for the projectCard links
+    constructor (){
+        super()
+        this.conversationalistClick = this.conversationalistClick.bind(this)
+    }
+
+conversationalistClick = () => {
+    window.open('https://github.com/ReganIu/Conversationalist')
+}
+
+
     render(){
         return (
             <div>
                 <div className='jumbotron-fluid' id="jumbotronProjects">
                     <h1 id="projectWord">Projects</h1>
+                    
+                    <div className="row">
+                        <div className="column">
+                            <Card>
+                                <Card.Img variant="top" src={'../../resources/background/picture.png'}/>
+                                <Card.Body>
+                                    <Card.Text> A text-to-speech Android application that helps users learn and improve their spoken language.</Card.Text>
+                                    <Button variant="primary" onClick={this.conversationalistClick}> Github </Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </div>
+
+
                 </div>
+                
             </div>
         )
     }
