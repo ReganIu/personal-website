4c79fae4c5db14e66edb100aaaf0d72ccdc22f5c
Regan Iu <reganiu@hotmail.com>
2020-12-17 01:05:36 -0500

    desktop to mobile textbox resizing changes

--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -15,8 +15,8 @@ class About extends React.Component {
                         <div class="col-sm-6">
                             <img id="me-photo" src={require('../../resources/portraits/me.jpg')} alt='me'/>
                         </div>
-                        <div class="col-sm-6">
-                            <h1 className="about-me" id="me-text">Hi, my name is Regan Iu and I'm currently a 3rd year student at the University of Toronto, where I'm majoring in Computational Cognition and double minoring in Mathematics and Statistics. I built this website in React to learn more about web development and to show others about the projects that I've created!</h1> 
+                        <div class="col-sm-6" id="aboutMe">
+                            <h3 className="about-me" id="me-text">Hi, my name is Regan Iu and I'm currently a third year student at the University of Toronto, where I'm majoring in Computational Cognition and double minoring in Mathematics and Statistics. I built this website in React to learn more about web development and to show others about the projects that I've created!</h3> 
                         </div>
                     </div>
                 </div>
