0125a3e9cc689783c8451d87e1ccdd3365660f96
Regan Iu <reganiu@hotmail.com>
2021-01-01 02:11:19 -0500

    styling changes

--- src/sections/NavBar/Navigation.js
+++ src/sections/NavBar/Navigation.js
@@ -1,48 +1,34 @@
 import React from 'react';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Pdf from "../../resources/ReganIuResume.pdf"
 import "./Navigation.css"
 
 
 class Navigation extends React.Component {
 
     // constructor method to bind the methods for the sticky Navbar 
-    // constructor (){
-    //     super()
-    //     this.myFunction = this.myFunction.bind(this)
-    // }
-
-    // function myFunction () {
-    // window.onscroll = function() {myFunction()};
-
-    // var navbar = document.getElementById("navbar");
-    // var sticky = navbar.offsetTop;
-
-    
-    //     if (window.pageYOffset >= sticky) {
-    //         navbar.classList.add("sticky")
-    //     } else {
-    //         navbar.classList.remove("sticky");
-    //     }
-    // }
 
 
     render () {
         return(
             <div>
             <Navbar collapseOnSelect id="navbar" expand="lg" bg="light" variant="light">
-            <Navbar.Brand href="#Intro">Regan Iu</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav id="navSections" className="mr-auto">
-                    <Nav.Link  id = "home" href="#intro" activeClass="active">Home</Nav.Link>
-                    <Nav.Link  id = "about" href="#about" >About</Nav.Link>
-                    <Nav.Link  id = "projects" href="#projects" >Projects</Nav.Link>
-                    <Nav.Link  id = "contact" href="#contact" >Contact</Nav.Link>
+                    <Nav.Link className="txt" href="#intro" activeClass="active">Home</Nav.Link>
+                    <Nav.Link className="txt" href="#about" >About</Nav.Link>
+                    <Nav.Link className="txt" href="#projects" >Projects</Nav.Link>
+                    <Nav.Link className="txt" href="#contact" >Contact</Nav.Link>
                 </Nav>
                 <Nav>
                     <div class="icons">
+                        <a id="resume-logo" href={Pdf} target = "blank">
+                            <i class="fas fa-file-alt"></i>
+                        </a>
+
                         <a id="linkedin-logo" href="https://www.linkedin.com/in/reganiu/" target ="blank">
                             <i class="fab fa-linkedin"></i>
                         </a>
