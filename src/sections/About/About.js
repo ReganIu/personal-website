c7f85b5f4d385a35d1a6b9ed793eea37d8f59a1c
Regan Iu <reganiu@hotmail.com>
2022-03-01 21:37:48 -0500

    Changed about me photo in js file

--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -13,7 +13,7 @@ class About extends React.Component {
                 <div class="container fill-screen-height"> 
                     <div class="row">
                         <div class="col-sm-6">
-                            <img id="me-photo" src={require('../../resources/portraits/me.jpg')} alt='me'/>
+                            <img id="me-photo" src={require('../../resources/portraits/me with tie.jpg')} alt='me'/>
                         </div>
                         <div class="col-sm-6" id="aboutMe">
                             <h3 className="about-me" id="me-text">Hi, my name is Regan Iu and I'm a student at the University of Toronto. I'm currently a third year student majoring in Computational Cognition and double minoring in Mathematics and Statistics. I built this website in React to learn more about web development and to show others about the projects that I've created!</h3> 
