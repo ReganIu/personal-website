49c3afd563ae7f3f542cf80062ac6539fe7fe1f4
Regan Iu <reganiu@hotmail.com>
2022-03-02 00:30:44 -0500

    Changed class to col-sm-4 so not squished in mobile

--- src/sections/Experience/Experience.js
+++ src/sections/Experience/Experience.js
@@ -11,14 +11,14 @@ class Experience extends React.Component {
                 <div className='jumbotron-fluid' id="jumbotronExp">
                     <h1 id="expWord">Experience</h1>
                     
-                    <div class="row h-100">
-                        <div class="col"></div>
-                        <div class="col-4">
+                    <div class="row">
+                        <div class="col-sm-4"></div>
+                        <div class="col-sm-4">
                             <Card>
                                 <img id="wattpadPic"  src={require('../../resources/Wattpad-Logo.png')} alt="wattpad-logo"/>
                                 <Card.Header style={{fontFamily:'Lato', textAlign: 'center', fontSize: '5vh', color: 'white'}}>Wattpad</Card.Header>
                                 <Card.Body>
-                                    <Card.Subtitle className="mb-2 text-muted">Associate Engineer </Card.Subtitle>
+                                    <Card.Subtitle className="mb-2 text-muted">Associate Engineer</Card.Subtitle>
                                     <Card.Text>
                                         • Creator Journey | May 2021 - Dec 2021
                                     </Card.Text>
@@ -28,7 +28,7 @@ class Experience extends React.Component {
                                 </Card.Body> 
                             </Card>
                         </div>
-                        <div class="col"></div>
+                        <div class="col-sm-4"></div>
                     </div>
                     
                 </div>
