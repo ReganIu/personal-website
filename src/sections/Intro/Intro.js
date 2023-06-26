39df42b905f4146d8c3c88b428574877865e8314
Regan Iu <reganiu@hotmail.com>
2023-06-26 18:45:07 -0400

    implemented framer-motion and testing with it

--- src/sections/Intro/Intro.js
+++ src/sections/Intro/Intro.js
@@ -1,8 +1,43 @@
 import React from 'react';
-import "./IntroAnimated.css"
+import "./IntroAnimated.css";
+import { motion } from "framer-motion/dist/framer-motion"; 
+
 
 class Intro extends React.Component {
-    render() {
+    state = {
+        isClicked: false,
+      };
+    
+      handleClick = () => {
+        this.setState((prevState) => ({
+          isClicked: !prevState.isClicked,
+        }));
+      };
+    render() {    
+        const nameVariants = {
+            hidden: {
+                opacity: 0,
+              },
+          visible: {
+            opacity: 1,
+            transition: {
+                duration: 5,
+                delay: 0.8,
+                staggerChildren: 0.3,
+            },
+          },
+        };
+
+        const letterVariants = {
+            hidden: {
+              opacity: 0,
+            },
+            visible: {
+              opacity: 1,
+            },
+          };
+
+        const name = "I'm Regan!";
 
 
         return (    
@@ -10,8 +45,24 @@ class Intro extends React.Component {
                 <div class="animated-title">
                     <div class="text-top">
                         <div>
-                            <span>Hey,</span>
-                            <span>I'm Regan!</span>
+                            <span> Hey, </span>
+                            <motion.h1
+                            className="intro-name"
+                            variants={nameVariants}
+                            initial="hidden"
+                            animate="visible"
+                            
+                            >
+                            {name.split("").map((letter, index) => (
+                            <motion.span
+                            key={index}
+                            variants={letterVariants}
+                            style={{ display: "inline", fontSize: "12vmin" }}
+                            >
+                                {letter}
+                            </motion.span>
+                            ))}
+                            </motion.h1>
                         </div>
                     </div>
                     <div class="text-bottom">
