378ba9ba23011220ddee55f836bbfb825882bfc3
Regan Iu <reganiu@hotmail.com>
2023-07-05 18:17:32 -0400

    added animations to about section

--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -44,9 +44,10 @@ class About extends React.Component {
                       <motion.h1
                           id="about-word"
                           variants={shakeVariants}
-                          animate={ isShaking ? "shaking" : "hidden" }
-                          // whileHover="shaking"
-                          whileTap="shaking"
+                          // animate={ isShaking ? "shaking" : "hidden" }
+                          animate={{ rotateY: 30 }}
+                          transition={{ repeat: Infinity, repeatType: "mirror", repeatDelay: 0.3 }}
+
                       >
                           About
                       </motion.h1>
@@ -55,10 +56,40 @@ class About extends React.Component {
               <div class="container fill-screen-height"> 
                   <div class="row">
                       <div class="col-sm-6">
-                          <img id="me-photo" src={require('../../resources/portraits/me.JPG')} alt='me'/>
+                          <motion.img 
+                          id="me-photo" 
+                          src={require('../../resources/portraits/me.JPG')} 
+                          alt='me'
+                          initial={{ opacity: 0, scale: 0.7 }}
+                          whileInView={{ opacity: 1, scale: 1 }}
+                          transition={{
+                            duration: 2,
+                            scale: {
+                              type: "spring",
+                              damping: 10,
+                              stiffness: 100,
+                              restDelta: 0.001
+                            }
+                          }}
+                          />
                       </div>
                       <div class="col-sm-6" id="aboutMe">
-                          <h3 className="about-me" id="me-text">Hey, I'm Regan Iu and I'm a recent graduate from the University of Toronto. I'm a passionate about creating engaging and user-friendly experiences. I strive to continuously learn and grow in this ever-evolving field, and I'm excited to collaborate on exciting projects. Scroll down to learn more about my experience and the projects that I've created!</h3> 
+                          <motion.h3 
+                          className="about-me" 
+                          id="me-text"
+                          initial={{ opacity: 0, x: 100 }}
+                          whileInView={{ opacity: 1, x: 0 }}
+                          transition={{
+                            duration: 2,
+                            scale: {
+                              type: "spring",
+                              damping: 5,
+                              stiffness: 100,
+                            }
+                          }}
+                          >
+                            Hey, I'm Regan Iu and I'm a recent graduate from the University of Toronto. I'm a passionate about creating engaging and user-friendly experiences. I strive to continuously learn and grow in this ever-evolving field, and I'm excited to collaborate on exciting projects. Scroll down to learn more about my experience and the projects that I've created!
+                          </motion.h3> 
                           </div>
                   </div>
               </div>
