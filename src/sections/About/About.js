5bffaba7ca8ec0d3a3980237886410cb903a204a
Regan Iu <reganiu@hotmail.com>
2023-07-06 16:00:03 -0400

    changed the text to fade in from bottom

--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -77,8 +77,8 @@ class About extends React.Component {
                           <motion.h3 
                           className="about-me" 
                           id="me-text"
-                          initial={{ opacity: 0, x: 100 }}
-                          whileInView={{ opacity: 1, x: 0 }}
+                          initial={{ opacity: 0, y: 100 }}
+                          whileInView={{ opacity: 1, y: 0 }}
                           transition={{
                             duration: 2,
                             scale: {
