8bf6933a1fef542c12ed1ef7343fac482f5b66fd
Regan Iu <reganiu@hotmail.com>
2023-07-17 00:22:43 -0400

    added same canvas and scene into about section

--- src/sections/Intro/Intro.js
+++ src/sections/Intro/Intro.js
@@ -61,6 +61,27 @@ const Intro = () => {
           );
     };
 
+    const Star2 = ({ p }) => {
+      const ref = useRef(null);
+    
+      useLayoutEffect(() => {
+        const distance = mix(3, 3, Math.random());
+        const yAngle = mix(
+          degreesToRadians(180),
+          degreesToRadians(10),
+          Math.random()
+        );
+        const xAngle = degreesToRadians(360) * p;
+        ref.current.position.setFromSphericalCoords(distance, yAngle, xAngle);
+      });
+      return (
+          <mesh ref={ref}>
+            <boxGeometry args={[0.05, 0.05, 0.05]} />
+            <meshBasicMaterial wireframe color={"#aee4ed"} />
+          </mesh>
+        );
+  };
+
     function Scene({ numStars = 1000 }) {
         const gl = useThree((state) => state.gl);
         const { scrollYProgress } = useScroll();
@@ -76,7 +97,7 @@ const Intro = () => {
           camera.position.setFromSphericalCoords(
             distance.get(),
             yAngle.get(),
-            time.get() * 0.0005
+            time.get() * 0.00025
           );
           camera.updateProjectionMatrix();
           camera.lookAt(0, 0, 0);
@@ -95,12 +116,60 @@ const Intro = () => {
           </>
         );
     }
+
+    function Scene2({ numStars = 1000 }) {
+      const gl = useThree((state) => state.gl);
+      const { scrollYProgress } = useScroll();
+      const yAngle = useTransform(
+        scrollYProgress,
+        [0, 1],
+        [0.001, degreesToRadians(360)]
+      );
+      const distance = useTransform(scrollYProgress, [0, 1], [10, 3]);
+      const time = useTime();
+    
+      useFrame(({ camera }) => {
+        camera.position.setFromSphericalCoords(
+          distance.get(),
+          yAngle.get(),
+          time.get() * 0.0005
+        );
+        camera.updateProjectionMatrix();
+        camera.lookAt(3, 0, 0);
+      });
+    
+      useLayoutEffect(() => gl.setPixelRatio(1));
+    
+      const stars = [];
+      for (let i = 2; i < numStars; i++) {
+        stars.push(React.createElement(Star2, { p: progress(0, numStars, i) }));
+      }
+    
+      return (
+        <>
+          {stars}
+        </>
+      );
+  }
   
     return (
       <div className="jumbotron-fluid " id="jumbotronIntro">
         <Canvas >
             <Scene />
         </Canvas>
+
+        <Canvas >
+            <Scene2 />
+        </Canvas>
+
+        {/* <Canvas >
+            <Scene />
+        </Canvas>
+        
+        <Canvas >
+            <Scene />
+        </Canvas> */}
+
         <div className="animated-title">           
           <div className="text-top">           
             <div>              
