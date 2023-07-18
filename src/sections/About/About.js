69d642906e4c22028e83236812c23a313073c242
Regan Iu <reganiu@hotmail.com>
2023-07-18 14:22:51 -0400

    changed about to functional component

--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -1,26 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './About.css'
 import { motion } from "framer-motion"
 
-class About extends React.Component {
+const About = () => {
     
-  constructor(props) {
-    super(props);
-    this.state = {
-        isShaking: true
-    };
-  }
+  const [isShaking, setIsShaking] = useState(true);
 
-  componentDidMount() {
-    setInterval(() => {
-        this.setState((prevState) => ({
-            isShaking: !prevState.isShaking
-        }));
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIsShaking((prevIsShaking) => !prevIsShaking);
     }, 1000);
-  }
 
-  render () {
-    const { isShaking } = this.state;
+    return () => clearInterval(intervalId);
+  }, []);
     
     const shakeVariants = {
       hidden: { 
@@ -36,81 +28,81 @@ class About extends React.Component {
       },
     };
 
-      return (
-          <div id='aboutSection' overflow="default">
-              
-              <div className='jumbotron-fluid' id="jumbotronAbout" ref={this.sectionRef}>
-                      <motion.h1
-                          id="about-word"
-                          variants={shakeVariants}
-                          // animate={ isShaking ? "shaking" : "hidden" }
-                          animate={{ rotateY: 30 }}
-                          transition={{ repeat: Infinity, repeatType: "reverse", repeatDelay: 0.3 }}
+    return (
+        <div id='aboutSection' overflow="default">
+            
+            <div className='jumbotron-fluid' id="jumbotronAbout" >
+                    <motion.h1
+                        id="about-word"
+                        variants={shakeVariants}
+                        // animate={ isShaking ? "shaking" : "hidden" }
+                        animate={{ rotateY: 30 }}
+                        transition={{ repeat: Infinity, repeatType: "reverse", repeatDelay: 0.3 }}
 
-                      >
-                          About
-                      </motion.h1>
-              </div>
+                    >
+                        About
+                    </motion.h1>
+            </div>
 
-              <div class="container fill-screen-height"> 
-                  <div class="row">
-                      <div class="col-sm-6">
-                          <motion.img 
-                          id="me-photo" 
-                          src={require('../../resources/portraits/me.JPG')} 
-                          alt='me'
-                          initial={{ opacity: 0, scale: 0.7 }}
-                          whileInView={{ opacity: 1, scale: 1 }}
-                          transition={{
-                            duration: 2,
-                            scale: {
-                              type: "spring",
-                              damping: 10,
-                              stiffness: 100,
-                              restDelta: 0.001
-                            }
-                          }}
-                          />
-                      </div>
-                      <div class="col-sm-6" id="aboutMe">
-                          <motion.h3 
-                          className="about-me" 
-                          id="me-text"
-                          initial={{ opacity: 0, y: 100 }}
-                          whileInView={{ opacity: 1, y: 0 }}
-                          transition={{
-                            duration: 2,
-                            scale: {
-                              type: "spring",
-                              damping: 5,
-                              stiffness: 100,
-                            }
-                          }}
-                          >
-                            Hey, I'm Regan Iu and I'm a recent graduate from the University of Toronto. I'm passionate about creating engaging and user-friendly experiences.
-                          </motion.h3> 
-                          <motion.h3 
-                          className="about-me" 
-                          id="me-text"
-                          initial={{ opacity: 0, y: 0 }}
-                          whileInView={{ opacity: 1, y: 1 }}
-                          transition={{
-                            duration: 2,
-                            scale: {
-                              type: "spring",
-                              damping: 5,
-                              stiffness: 100,
-                            }
-                          }}
-                          >
-                            I strive to continuously learn and grow in this ever-evolving field, and I'm excited to collaborate on exciting projects. Scroll down to learn more about my experiences and the projects that I've created!
-                          </motion.h3> 
-                          </div>
-                  </div>
-              </div>
-          </div>         
-      )
-  }
+            <div class="container fill-screen-height"> 
+                <div class="row">
+                    <div class="col-sm-6">
+                        <motion.img 
+                        id="me-photo" 
+                        src={require('../../resources/portraits/me.JPG')} 
+                        alt='me'
+                        initial={{ opacity: 0, scale: 0.7 }}
+                        whileInView={{ opacity: 1, scale: 1 }}
+                        transition={{
+                          duration: 2,
+                          scale: {
+                            type: "spring",
+                            damping: 10,
+                            stiffness: 100,
+                            restDelta: 0.001
+                          }
+                        }}
+                        />
+                    </div>
+                    <div class="col-sm-6" id="aboutMe">
+                        <motion.h3 
+                        className="about-me" 
+                        id="me-text"
+                        initial={{ opacity: 0, y: 100 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{
+                          duration: 2,
+                          scale: {
+                            type: "spring",
+                            damping: 5,
+                            stiffness: 100,
+                          }
+                        }}
+                        >
+                          Hey, I'm Regan Iu and I'm a recent graduate from the University of Toronto. I'm passionate about creating engaging and user-friendly experiences.
+                        </motion.h3> 
+                        <motion.h3 
+                        className="about-me" 
+                        id="me-text"
+                        initial={{ opacity: 0, y: 0 }}
+                        whileInView={{ opacity: 1, y: 1 }}
+                        transition={{
+                          duration: 2,
+                          scale: {
+                            type: "spring",
+                            damping: 5,
+                            stiffness: 100,
+                          }
+                        }}
+                        >
+                          I strive to continuously learn and grow in this ever-evolving field, and I'm excited to collaborate on exciting projects. Scroll down to learn more about my experiences and the projects that I've created!
+                        </motion.h3> 
+                        </div>
+                </div>
+            </div>
+        </div>         
+    )
+  
 }
 
 export default About
